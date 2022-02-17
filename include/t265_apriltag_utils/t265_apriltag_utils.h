d1c092845f93638ca3452db692fc9d077e775e2b
Cedric GOUBARD <c.goubard21@imperial.ac.uk>
2022-02-17 14:45:35 +0000

    Link library

---
+++ include/t265_apriltag_utils/t265_apriltag_utils.h
@@ -0,0 +1,18 @@
+#include <librealsense2/rs.hpp>
+#include <librealsense2/rsutil.h>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <chrono>
+#include <future>
+#include <math.h>
+
+#include <sensor_msgs/CameraInfo.h>
+#include <ros/ros.h>
+
+#include <apriltag/apriltag.h>
+#include <apriltag/tag36h11.h>
+#include <apriltag/apriltag_pose.h>
+#include <apriltag/common/homography.h>
+
+rs2_intrinsics get_rs2_int_from_camera_info(const sensor_msgs::CameraInfoConstPtr& caminf);
--- src/t265_apriltag.cpp
+++ src/t265_apriltag.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <sensor_msgs/Image.h>
 #include <std_msgs/Header.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <geometry_msgs/Pose.h>
 #include <ros/ros.h>
+#include <message_filters/subscriber.h>
 #include <tf/transform_broadcaster.h>
 #include <image_transport/image_transport.h>
+
+
+#include <librealsense2/rs.hpp>
+#include <librealsense2/rsutil.h>
+#include <t265_apriltag_utils/t265_apriltag_utils.h>
 
 using namespace std;
 
@@ -18,21 +25,32 @@ private:
     ros::NodeHandle nh;
     image_transport::ImageTransport it;
     image_transport::Subscriber sub;
+    ros::Subscriber info_sub;
 
     tf::TransformBroadcaster br;
     tf::Transform latest_tf;
 
 public:
-    Detector(): nh(ros::NodeHandle("~")), it(nh)
+    Detector(): nh(ros::NodeHandle("~")), it(nh) 
     {
-        std::string img_topic;
+        std::string img_topic, camera_info_topic;
         this->nh.param<std::string>("image", img_topic, "/t265/fisheye1/image_raw");
         this->nh.param<std::string>("parent", this->parent_frame, "t265_fisheye1_optical_frame");
         this->nh.param<std::string>("child", this->child_frame, "t265_tag");
+        this->nh.param<std::string>("camera_info", camera_info_topic, "/t265/fisheye1/camera_info");
 
         this->sub = this->it.subscribe(img_topic, 100, &Detector::imgCallback, this);
+        this->info_sub = this->nh.subscribe(camera_info_topic, 100, &Detector::infoCallback, this);
 
     }
+
+    
+    void infoCallback(const sensor_msgs::CameraInfoConstPtr& info_msg)
+    {
+        rs2_intrinsics rsint = get_rs2_int_from_camera_info(info_msg);
+
+    }
+    
 
     void imgCallback(const sensor_msgs::Image::ConstPtr &img)
     {
